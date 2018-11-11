9785a22cca46dd802ee276a2b5198e1720ca8773
Deddy Kosasih <dkosasih@gmail.com>
2018-11-11 22:37:45 +1100

    fix(autofac): move autofac orchestration to API project only

---
+++ DestinationV/DestinationV.Api/Modules/ApplicationModule.cs
@@ -0,0 +1,45 @@
+﻿using System.Reflection;
+using Autofac;
+using Autofac.Features.Variance;
+using DestinationV.Application.Commands;
+using MediatR;
+
+namespace DestinationV.Api.Modules
+{
+    public class ApplicationModule : Autofac.Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+
+            builder.RegisterAssemblyTypes(ThisAssembly)
+                .AsImplementedInterfaces();
+
+            // enables contravariant Resolve() for interfaces with single contravariant ("in") arg
+            builder
+                .RegisterSource(new ContravariantRegistrationSource());
+
+            // mediator itself
+            builder
+                .RegisterType<Mediator>()
+                .As<IMediator>()
+                .InstancePerLifetimeScope();
+
+            // Register all the Command classes (they implement IRequestHandler) in assembly holding the Commands
+            builder.RegisterAssemblyTypes(typeof(RouteCommand).GetTypeInfo().Assembly)
+                .AsClosedTypesOf(typeof(IRequestHandler<,>));
+
+            // Implement INotifierHandler and IValidator when first Notifier or Validator is being implemented
+
+            // request handlers
+            builder.Register<ServiceFactory>(context =>
+            {
+                var componentContext = context.Resolve<IComponentContext>();
+                return t =>
+                {
+                    object o;
+                    return componentContext.TryResolve(t, out o) ? o : null;
+                };
+            });
+        }
+    }
+}
---
+++ DestinationV/DestinationV.Api/Modules/RepositoryModule.cs
@@ -0,0 +1,16 @@
+﻿using Autofac;
+using DestinationV.Domain.Entity.PlaceEntity;
+using DestinationV.Domain.Entity.RouteEntity;
+using DestinationV.Persistence.Repositories;
+
+namespace DestinationV.Api.Modules
+{
+    public class RepositoryModule : Autofac.Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<RouteRepository>().As<IRouteRepository>().InstancePerLifetimeScope();
+            builder.RegisterType<PlaceRepository>().As<IPlaceRepository>().InstancePerLifetimeScope();
+        }
+    }
+}
