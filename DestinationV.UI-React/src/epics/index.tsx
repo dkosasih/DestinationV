011ed581ada5a94cbb8e2db99389e0d736ef2fba
Deddy Kosasih <dkosasih@gmail.com>
2018-11-25 21:59:24 +1100

    feat(framework): add redux-observable scaffolding

---
+++ DestinationV.UI-React/src/epics/index.tsx
@@ -0,0 +1,6 @@
+import { combineEpics } from "redux-observable";
+
+const epics = combineEpics(
+);
+
+export default epics;
---
+++ DestinationV.UI-React/src/reducers/index.tsx
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux';
+
+export type RootState = {
+};
+
+const reducers = combineReducers({
+});
+
+export default reducers;
