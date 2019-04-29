75db47b6d8a48d33b92ccbc3dcadfa236d5e1868
Deddy Kosasih <deddy.kosasih@vline.com.au>
2019-04-29 22:50:20 +1000

    feat(docker): add clean database running on docker

---
+++ DestinationV/DestinationV.Database/dbo/Tables/Routes.sql
@@ -0,0 +1,20 @@
+﻿CREATE TABLE [dbo].[Routes] (
+    [Id]            NVARCHAR (450) NOT NULL,
+    [OriginId]      NVARCHAR (450) NULL,
+    [DestinationId] NVARCHAR (450) NULL,
+    [DepartUtc]     DATETIME       NOT NULL,
+    CONSTRAINT [PK_Routes] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Routes_Places_DestinationId] FOREIGN KEY ([DestinationId]) REFERENCES [dbo].[Places] ([Id]),
+    CONSTRAINT [FK_Routes_Places_OriginId] FOREIGN KEY ([OriginId]) REFERENCES [dbo].[Places] ([Id])
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_Routes_DestinationId]
+    ON [dbo].[Routes]([DestinationId] ASC);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_Routes_OriginId]
+    ON [dbo].[Routes]([OriginId] ASC);
+
